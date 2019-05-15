5e3049f4d620e872c2c69963f91403b406a290c2
cottephi <cottephi@gmail.com>
2019-05-15 16:44:28 +0200

    Chap 3 v1 done

--- Chapitre_3/pictures/svg_to_pdf.sh
+++ Chapitre_3/pictures/svg_to_pdf.sh
@@ -1,12 +1,12 @@
 for afile in *.svg ; do
   afile_png=${afile/.svg/.png}
   afile_pdf=${afile/.svg/.pdf}
-  if [ -e $afile_png ] ; then
-    rm $afile_png
-    echo "Removed $afile_png"
-  fi
   if [ ! -e $afile_pdf ] ; then
     inkscape --file=$afile --export-area-drawing --without-gui --export-pdf=$afile_pdf
     echo "Created $afile_pdf"
+    if [ -e $afile_png ] ; then
+      rm $afile_png
+      echo "Removed $afile_png"
+    fi
   fi
 done
